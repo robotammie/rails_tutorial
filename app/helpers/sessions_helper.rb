6c5ace894fdaba64b3f3fe44179056aabdef7bda
robotammie <tammie.kahnhauser@gmail.com>
2020-12-15 22:30:42 -0500

    9.1.3 Forgetting Users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,12 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
@@ -30,6 +36,7 @@ module SessionsHelper
 
   # Logs out the current user.
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil
   end
