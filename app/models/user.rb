c68248e37fbff5b6226fcca4a3e7beba65b7799b
robotammie <tammie.kahnhauser@gmail.com>
2020-12-30 22:07:11 -0800

    11.3.3 Activation Test and Refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,6 +55,16 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+  # Activates an account.
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
     # Converts email to all lower-case.
