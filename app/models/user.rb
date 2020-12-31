9bec7e47ad965c2913651da18622191424b507ba
robotammie <tammie.kahnhauser@gmail.com>
2020-12-30 19:25:48 -0800

    11.1.2 Account Activation Data Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { email.downcase! }
+  before_create :create_activation_digest
+
+  before_save :downcase_email
 
   validates :name,     presence: true,
                        length: { maximum: 50 }
@@ -51,4 +53,17 @@ class User < ApplicationRecord
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
+  private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email.downcase!
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
